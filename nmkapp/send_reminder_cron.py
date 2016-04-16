6d095375bbedd09f4427c14dd2d7edc20258d982
branko <branko.kokanovic@gmail.com>
2016-04-16 22:48:57 +0200

    fixovanje cron job-ova

--- nmkapp/send_reminder_cron.py
+++ nmkapp/send_reminder_cron.py
@@ -10,10 +10,12 @@ import os
 import sys
 import datetime
 import logging
+import django
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nmk.settings")
 sys.path.append(os.path.abspath(os.path.dirname(__file__)) + "/../")
 
-from nmk import settings
+django.setup()
 #####################################################################
 
 from nmkapp.models import Round, Match, Shot, Player
@@ -26,33 +28,30 @@ from django.template import loader, Context
 logger = logging.getLogger(__name__)
 
 def get_rounds_starting_tomorrow():
-    current_time = datetime.datetime.now()
-    previous_time = current_time - datetime.timedelta(minutes=15)
-    
     rounds = Round.objects.filter(active=True)
     rounds_to_return = []
-    for round in rounds:
-        min_time = Match.objects.filter(round=round).aggregate(Min('start_time'))['start_time__min']
+    for nmk_round in rounds:
+        min_time = Match.objects.filter(round=nmk_round).aggregate(Min('start_time'))['start_time__min']
         if min_time == None:
             continue            
         hours_diff = (min_time - datetime.datetime.now()).total_seconds() / (60 * 60)
         if hours_diff >= 23.5 and hours_diff < 24.5:
-            rounds_to_return.append([round, min_time])
+            rounds_to_return.append([nmk_round, min_time])
     return rounds_to_return
 
-def send_reminder_for_round(round, min_time):
+def send_reminder_for_round(nmk_round, min_time):
     all_players = Player.objects.exclude(user__email = "").filter(send_mail=True).filter(user__is_active=True)
     player_mails = []
     for player in all_players:
-        if not Shot.objects.filter(user_round__round=round).filter(user_round__user=player.user).exists():
+        if not Shot.objects.filter(user_round__round=nmk_round).filter(user_round__user=player.user).exists():
             player_mails.append(player.user.email)
             
     template = loader.get_template("mail/round_reminder.html")
-    message_text = template.render(Context({"round": round, "min_time": min_time}))
+    message_text = template.render(Context({"round": nmk_round, "min_time": min_time}))
     if settings.SEND_MAIL:
-        logger.info("Sending mail that round %s will be starting soon to %s", round.name, player_mails)
+        logger.info("Sending mail that round %s will be starting soon to %s", nmk_round.name, player_mails)
         for mail in player_mails:
-            msg = EmailMessage(u"[nmk] Podsetnik za kolo \"%s\"" % (round.name), message_text, "nmk@kokanovic.org", to=[mail,])
+            msg = EmailMessage(u"[nmk] Podsetnik za kolo \"%s\"" % (nmk_round.name), message_text, "nmk@kokanovic.org", to=[mail,])
             msg.content_subtype = "html"
             msg.send(fail_silently = False)
 
--- nmkapp/send_shots_cron.py
+++ nmkapp/send_shots_cron.py
@@ -10,10 +10,11 @@ import os
 import sys
 import datetime
 import logging
+import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nmk.settings")
 sys.path.append(os.path.abspath(os.path.dirname(__file__)) + "/../")
 
-from nmk import settings
+django.setup()
 #####################################################################
 
 from nmkapp.models import Round, Match, UserRound, Shot, Player
@@ -30,18 +31,18 @@ def get_rounds_just_started():
     
     rounds = Round.objects.all()
     rounds_to_return = []
-    for round in rounds:
-        matches1 = Match.objects.all().filter(round=round).filter(start_time__lte=current_time).count()
-        matches2 = Match.objects.all().filter(round=round).filter(start_time__lte=previous_time).count()
+    for nmk_round in rounds:
+        matches1 = Match.objects.all().filter(round=nmk_round).filter(start_time__lte=current_time).count()
+        matches2 = Match.objects.all().filter(round=nmk_round).filter(start_time__lte=previous_time).count()
         if (matches1 > 0) and (matches2 == 0):
-            rounds_to_return.append(round)
+            rounds_to_return.append(nmk_round)
     return rounds_to_return
 
-def send_mail_for_round(round):
-    matches = list(Match.objects.filter(round=round).order_by("start_time", "id"))
+def send_mail_for_round(nmk_round):
+    matches = list(Match.objects.filter(round=nmk_round).order_by("start_time", "id"))
     round_standings = []
     
-    user_rounds = UserRound.objects.filter(round=round).order_by("user__first_name", "user__last_name")
+    user_rounds = UserRound.objects.filter(round=nmk_round).order_by("user__first_name", "user__last_name")
     for user_round in user_rounds:
         shots = list(Shot.objects.filter(user_round=user_round).order_by("match__start_time", "match"))
         round_standings.append({"user_round": user_round, "shots": shots})
@@ -49,11 +50,11 @@ def send_mail_for_round(round):
     all_players = Player.objects.all()
     all_user_mail = [player.user.email for player in all_players if player.send_mail==True and player.user.email != "" and player.user.is_active==True]
     template = loader.get_template("mail/round_shots.html")
-    message_text = template.render(Context({"round": round, "matches": matches, "round_standings": round_standings}))
+    message_text = template.render(Context({"round": nmk_round, "matches": matches, "round_standings": round_standings}))
     if settings.SEND_MAIL:
-        logger.info("Sending mail that round %s started to %s", round.name, all_user_mail)
+        logger.info("Sending mail that round %s started to %s", nmk_round.name, all_user_mail)
         for user_mail in all_user_mail:
-            msg = EmailMessage(u"[nmk] Počelo kolo \"%s\"" % (round.name), message_text, "nmk@kokanovic.org", to=[user_mail,])
+            msg = EmailMessage(u"[nmk] Počelo kolo \"%s\"" % (nmk_round.name), message_text, "nmk@kokanovic.org", to=[user_mail,])
             msg.content_subtype = "html"
             msg.send(fail_silently = False)
 
@@ -61,5 +62,5 @@ if __name__ == '__main__':
     logger.info("Starting check to see if any round is started")
     rounds = get_rounds_just_started()
     logger.info("Started rounds %s", rounds)
-    for round in rounds:
-        send_mail_for_round(round)
+    for nmk_round in rounds:
+        send_mail_for_round(nmk_round)
