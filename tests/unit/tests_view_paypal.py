d9f98c8343e7d499d6739ea30a4baf2840c14e4d
Branko Kokanovic <branko@kokanovic.org>
2018-04-20 22:01:59 +0200

    paypal test

---
+++ tests/unit/tests_view_paypal.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from django.test import Client
+from django.urls import reverse
+
+from nmkapp import views
+from nmkapp import models
+
+from .nmk_unit_test_case import NmkUnitTestCase
+
+
+class PaypalTests(NmkUnitTestCase):
+    fixtures = ['initial_data.json']
+
+    def test_anon_user(self):
+        client = Client()
+        response = client.get(reverse(views.paypal))
+        self.assertEqual(response.status_code, 200)
+
+    def test_regular_uer(self):
+        """
+        Test visiting paypal page
+        """
+        player = models.Player.objects.filter(user__id=1)[0]
+        self.assertFalse(player.in_money)
+
+        response = self.client.get(reverse(views.paypal))
+        self.assertEqual(response.status_code, 200)
+
+        player = models.Player.objects.filter(user__id=1)[0]
+        self.assertTrue(player.in_money)
