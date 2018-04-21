775dff1f5d6c2c3e51c551dc7d59d32a63b1ba54
Branko Kokanovic <branko@kokanovic.org>
2018-04-21 22:48:44 +0200

    test propositions view

---
+++ tests/unit/tests_view_propositions.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from django.test import Client
+from django.urls import reverse
+
+from nmkapp import views
+
+from .nmk_unit_test_case import NmkUnitTestCase
+
+
+class PropositionsTests(NmkUnitTestCase):
+    def test_anon_user(self):
+        client = Client()
+        response = client.get(reverse(views.proposition))
+        self.assertEqual(response.status_code, 200)
+
+    def test_regular_user(self):
+        """
+        Test proposition view
+        """
+        response = self.client.get(reverse(views.home))
+        self.assertEqual(response.status_code, 200)
