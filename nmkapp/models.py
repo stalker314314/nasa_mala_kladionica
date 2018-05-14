4af8b6568954afbeac03d8d5b2757cfa65ca0eb4
stalker314314 <branko@kokanovic.org>
2018-05-14 12:06:29 +0200

    Another case of not using lazy translations

--- nmkapp/models.py
+++ nmkapp/models.py
@@ -3,7 +3,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from timezone_field import TimeZoneField
 
