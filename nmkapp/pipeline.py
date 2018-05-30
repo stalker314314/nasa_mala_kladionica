5a42f8ae887da637c2229967fc2b8484a27579bc
Branko Kokanovic <branko@kokanovic.org>
2018-05-30 22:06:25 +0200

    Translation and touching third party auth a bit

--- nmkapp/pipeline.py
+++ nmkapp/pipeline.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from social_core.pipeline.partial import partial
-
-from django.urls import reverse
 from django.shortcuts import redirect
+from django.urls import reverse
+from social_core.pipeline.partial import partial
 
 from nmkapp.views import request_display_name
 
 
-""" Pauses PSA's pipeline to require display_name for users registering
-    via 3rd-party(Google, Twitter, etc.)
-
-    Returns redirect to a view that requires display_name from user
-    or None if display_name is already set(which subsequently continues the pipeline)"""
 @partial
 def request_display_name(strategy, details, user=None, is_new=False, *args, **kwargs):
+    """
+    Pauses PSA's pipeline to require display_name for users registering via 3rd-party(Google, Twitter, etc.)
+
+    Returns redirect to a view that requires display_name from user
+    or None if display_name is already set (which subsequently continues the pipeline)
+    """
     if user and user.first_name:
         return
     else:
