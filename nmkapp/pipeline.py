085f5165bbeeabb40f7a9fe2a9ea0e4711273064
Reinboo <nivalis@protonmail.com>
2018-05-30 19:41:49 +0200

    multi-line comm

--- nmkapp/pipeline.py
+++ nmkapp/pipeline.py
@@ -5,11 +5,11 @@ from django.urls import reverse
 from django.shortcuts import redirect
 
 
-# Pauses PSA's pipeline to require display_name for users registering
-# via 3rd-party(Google, Twitter, etc.)
-#
-# Returns redirect to a view that requires display_name from user
-# or None if display_name is already set(which subsequently continues the pipeline)
+""" Pauses PSA's pipeline to require display_name for users registering
+    via 3rd-party(Google, Twitter, etc.)
+
+    Returns redirect to a view that requires display_name from user
+    or None if display_name is already set(which subsequently continues the pipeline)"""
 @partial
 def request_display_name(strategy, details, user=None, is_new=False, *args, **kwargs):
     if user and user.first_name:
